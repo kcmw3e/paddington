41c8f4d768f3ce914c56611251a00569730e3a57
Casey Walker <kcmw3e@gmail.com>
2024-03-25 22:09:40 -0400

    Add CSV data logger

---
+++ teensy/src/paddington/lumberyard.cxx
@@ -0,0 +1,60 @@
+// Casey Walker (2024)
+// 24-673 Soft Robotics
+// Carnegie Mellon University
+// -------------------------------------------------------------------------- //
+#include <Arduino_DebugUtils.h>
+
+#include "lumberyard.h"
+
+#define CSV_LINE_BUF_LEN 1024
+
+
+bool Lumberyard::setup() {
+    if (!SD.begin(SD_CARD))
+        return false;
+
+    this->root = SD.open("/");
+    if (!this->root)
+        return false;
+
+    return true;
+}
+
+void Lumberyard::ls() {
+    DEBUG_INFO("Listing files and directories:");
+    for (File entry = this->root.openNextFile(); entry;
+         entry      = this->root.openNextFile()) {
+        if (entry.isDirectory())
+            DEBUG_INFO("> |--> ['%s']", entry.name());
+        else
+            DEBUG_INFO("> |--> '%s'", entry.name());
+    }
+    this->root.rewindDirectory();
+}
+
+void Lumberyard::change_savefile_to(const char* filepath) {
+    this->savefile = SD.open(filepath, FILE_WRITE);
+    if (!this->savefile) {
+        DEBUG_WARNING("Could not open file '%s'", filepath);
+    }
+
+    if (this->savefile.isDirectory()) {
+        DEBUG_WARNING("'%s' is a directory.", filepath);
+        this->savefile = File();
+    }
+}
+
+void Lumberyard::save_csv_line(uint32_t val1, uint32_t val2) {
+    if (!this->savefile)
+        return;
+
+    if (this->savefile.isDirectory())
+        return;
+
+    char buf[CSV_LINE_BUF_LEN];
+    auto n = snprintf(
+        buf, sizeof(buf) / sizeof(*buf), "%ld,%d,%d\n", micros(), val1, val2
+    );
+    this->savefile.write(buf, n);
+    this->savefile.flush();
+}
---
+++ teensy/src/paddington/lumberyard.h
@@ -0,0 +1,50 @@
+// Casey Walker (2024)
+// 24-673 Soft Robotics
+// Carnegie Mellon University
+// -------------------------------------------------------------------------- //
+#ifndef LUMBERYARD_H
+#define LUMBERYARD_H
+
+#include <string>
+
+#include <Arduino.h>
+#include <SD.h>
+#include <SPI.h>
+
+using namespace std;
+
+#define SD_CARD BUILTIN_SDCARD
+
+
+/// @brief The CSV data logger.
+class Lumberyard {
+  private:
+    File root;     /// @brief The root directory of the SD card.
+    File savefile; /// @brief The current save file.
+  public:
+    /// @brief Whether or not to currently write data to the save file.
+    bool should_save_data = false;
+
+    /// @brief Set up the data logger. For Arduino, this should be called in
+    ///   the `setup` function.
+    ///
+    /// @return `true` if successful setup, `false` otherwise.
+    bool setup();
+
+    /// @brief List the files and directories in the root directory of the SD
+    ///   card.
+    void ls();
+
+    /// @brief Change the file to which data is saved.
+    ///
+    /// @param `filepath` The new path to the file to save data to.
+    void change_savefile_to(const char* filepath);
+
+    /// @brief Save a CSV line in the current save file.
+    ///
+    /// @param `val1` The value read from the first sensor.
+    /// @param `val2` The value read from the second sensor.
+    void save_csv_line(uint32_t val1, uint32_t val2);
+};
+
+#endif // LUMBERYARD_H
